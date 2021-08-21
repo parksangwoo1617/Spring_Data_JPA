2a8e7ecdaf79fc2ec69ce82162dee3d773dd8484
parksangwoo <b01056721617@gmail.com>
2021-08-21 17:33:12 +0900

    📑 :: 기본 구조

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,19 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberJpaRepositoryTest {
+
+    @Autowired MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+    }
+}
