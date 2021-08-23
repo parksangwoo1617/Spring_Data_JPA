c74f08100e9c786eaffcfb750055a308d79f4321
parksangwoo <b01056721617@gmail.com>
2021-08-23 20:18:12 +0900

    ⚡ :: 커스텀 메소드

--- src/main/java/study/datajpa/repository/MemberRepositoryCustom.java
+++ src/main/java/study/datajpa/repository/MemberRepositoryCustom.java
@@ -1,2 +1,9 @@
-package study.datajpa.repository;public interface MemberRepositoryCustom {
+package study.datajpa.repository;
+
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+    List<Member> findMemberCustom();
 }
