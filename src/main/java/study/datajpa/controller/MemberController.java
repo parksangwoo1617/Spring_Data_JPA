f9c4404e6eb8c1d88ac66a5948faf129be2b7d31
parksangwoo <b01056721617@gmail.com>
2021-08-24 19:52:58 +0900

    ⚡ :: Paging, Sort 로직 추가

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,2 +1,43 @@
-package study.datajpa.controller;public class MemberController {
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}") // 도메인 클래스 컨버터
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUsername();
+    }
+
+    @GetMapping("/members") // Paging, Sort 메서드, Paging뿐만 아니라 Sort 옵션도 사용 가능
+    public Page<MemberDto> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
 }
